c4580a82ea70bcfa7d5d63cf30b8b2be2318bf2e
Tormod <tormod.fjeldskaar@gmail.com>
2010-07-31 00:22:12 +0200

    Added Visual Studio solution, along with solution to Problem 25

---
+++ Euler/Euler/Problem25.fs
@@ -0,0 +1,19 @@
+﻿module Problem25
+
+open System.Numerics
+
+let Solve = 
+
+    let limit = BigInteger.Pow(BigInteger 10, 999)
+
+    let fibonacci =
+        Seq.unfold
+            (fun (current, next) -> Some(current, (next, current + next)))
+            (BigInteger 0, BigInteger 1)
+
+    let term =
+        fibonacci
+        |> Seq.takeWhile (fun n -> n < limit)
+        |> Seq.length
+
+    printfn "%d" term
---
+++ Euler/Euler/Program.fs
@@ -0,0 +1,5 @@
+﻿// Learn more about F# at http://fsharp.net
+
+Problem25.Solve |> ignore
+
+System.Console.ReadKey() |> ignore
