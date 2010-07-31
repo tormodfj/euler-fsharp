b9ad976026fded0af60d40f0d71eff2df93319cc
Tormod <tormod.fjeldskaar@gmail.com>
2010-07-31 11:16:38 +0200

    Added solution to problem 3

---
+++ Euler.Lib/Primes.fs
@@ -0,0 +1,14 @@
+﻿module Primes
+
+let isPrime n =
+    let rec check i =
+        i > (n / 2) || (n % i <> 0 && check (i + 1))
+    check 2
+
+let factorize n =
+    let rec factorize n j factors =
+        match n with
+        | 1L -> factors
+        | x when (n % j) = 0L -> factorize (n/j) j (j::factors)
+        | x -> factorize n (j + 1L) (factors)
+    factorize n 2L []
---
+++ Euler/Problem3.fs
@@ -0,0 +1,9 @@
+﻿module Problem3
+
+let Solve =
+
+    let largestPrimeFactor = 
+        Primes.factorize 600851475143L
+        |> List.max
+
+    printfn "%d" largestPrimeFactor
